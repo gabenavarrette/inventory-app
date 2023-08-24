9eb3af87c0f0242770c600dfdb53c60b7355591b
Gabe Navarrette <gabenavarrette@gmail.com>
2023-08-24 13:49:15 -0500

    Fixed issues

--- inventory-app/src/components/InventoryForm.js
+++ inventory-app/src/components/InventoryForm.js
@@ -1,8 +1,4 @@
 import React, { useState } from "react";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
 
 const InventoryForm = ({ onAddItem }) => {
   const [itemName, setItemName] = useState("");
@@ -24,64 +20,56 @@ const InventoryForm = ({ onAddItem }) => {
     setItemName("");
     setItemQuantity("");
     setItemPrice("");
-    setShowSnackbar(true);
-  };
-
-  const handleSnackbarClose = () => {
-    setShowSnackbar(false);
+    setShowSnackbar(true); // Show snackbar after adding item
+    setTimeout(() => setShowSnackbar(false), 2000); // Hide snackbar after 2 seconds
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <TextField
-        className="form-input"
-        label="Item Name"
-        variant="outlined"
-        value={itemName}
-        onChange={(e) => setItemName(e.target.value)}
-        required
-      />
-      <TextField
-        className="form-input"
-        label="Quantity"
-        variant="outlined"
-        type="number"
-        value={itemQuantity}
-        onChange={(e) => setItemQuantity(e.target.value)}
-        required
-      />
-      <TextField
-        className="form-input"
-        label="Price"
-        variant="outlined"
-        type="number"
-        step="0.01"
-        value={itemPrice}
-        onChange={(e) => setItemPrice(e.target.value)}
-        required
-      />
-      <Button
-        className="form-button"
-        variant="contained"
-        color="primary"
-        type="submit"
-      >
+      <label>
+        Item Name:
+        <input
+          className="form-input"
+          type="text"
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Quantity:
+        <input
+          className="form-input"
+          type="number"
+          value={itemQuantity}
+          onChange={(e) => setItemQuantity(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Price:
+        <input
+          className="form-input"
+          type="number"
+          step="0.01"
+          value={itemPrice}
+          onChange={(e) => setItemPrice(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <button className="form-button" type="submit">
         Add Item
-      </Button>
-      <Snackbar
-        open={showSnackbar}
-        autoHideDuration={2000}
-        onClose={handleSnackbarClose}
-      >
-        <MuiAlert
-          elevation={6}
-          variant="filled"
-          severity="success"
-          onClose={handleSnackbarClose}
-        >
-          Item added successfully!
-        </MuiAlert>
-      </Snackbar>
+      </button>
+      {showSnackbar && (
+        <div className="snackbar">
+          <div className="alert success">
+            Item added successfully!
+          </div>
+        </div>
+      )}
     </form>
   );
 };
